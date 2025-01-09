bdf3b09ca7be8397c39deaf4c20b740be85dea01
Jayhan <han077han@naver.com>
2025-01-09 19:55:17 +0900

    lv.2 calculator

--- src/main/java/com/week3/Main.java
+++
@@ -1,9 +0,0 @@
-package com.week3;
-
-public class Main {
-    public static void main(String[] args) {
-        Car[] carArray = new Car[3];
-        Car car1 = new Car();
-        car1.changeGear( 'P');
-    }
-}
